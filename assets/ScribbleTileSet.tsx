2d8d13d0dba9cdf81de5d707fd5d96c9aab90320
rublormar <a21rublormar@inspedralbes.cat>
2024-01-30 13:26:04 +0100

    map

---
+++ assets/ScribbleTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="ScribbleTileSet" tilewidth="32" tileheight="32" tilecount="484" columns="22">
+ <image source="Spritesheet/spritesheet_default.png" width="704" height="704"/>
+</tileset>
