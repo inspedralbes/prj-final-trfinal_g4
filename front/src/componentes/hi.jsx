dab02d4c4f361e0a3fea055b908c8cda5610bbed
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2024-02-07 09:50:26 +0100

    hacer que el hi se muestre mas de un usuario

--- front/src/componentes/hi.jsx
+++ front/src/componentes/hi.jsx
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from 'react';
-export const Hi = (props) => {
-   
-    const username = localStorage.getItem('username');
-    return (
 
+export const Hi = (props) => {
+    const [usernames, setUsernames] = useState([]);
+
+    useEffect(() => {
+        const storedUsernames = localStorage.getItem('usernames');
+        if (storedUsernames) {
+            setUsernames(JSON.parse(storedUsernames));
+        }
+    }, []);
+
+    return (
         <div>
-            <h1>Hi {username}</h1>
+            {usernames.map((username, index) => (
+                <h1 key={index}>Hi {username}</h1>
+            ))}
         </div>
-    )
-}
+    );
+};
+            
