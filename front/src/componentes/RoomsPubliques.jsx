04c76a4ea9ba56fe70fce2b2d288b50370624e60
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2024-02-16 00:11:14 +0100

    no da error

--- front/src/componentes/RoomsPubliques.jsx
+++ front/src/componentes/RoomsPubliques.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import io from 'socket.io-client';
 
-export const RoomsPubliques = () => {
-    const [email, setEmail] = useState("");
-    const [pass, setPass] = useState("");
+ export const RoomsPubliques = () => {
     const [roomName, setRoomsName] = useState("");
     const [rooms, setRooms] = useState([]);
     const [selectedRoom, setSelectedRoom] = useState(null);
@@ -25,34 +23,9 @@ export const RoomsPubliques = () => {
     //     socket.emit('join', room);
     // };
 
-    const handleJoinRoom = async () => {
-        try {
-            const urlStrapi = 'http://localhost:1337/api/auth/local';
-            
-            const response = await fetch('http://localhost:1337/api/auth/local', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    identifier: email,
-                    password: pass,
-                }),
-            });
-
-            if (response.ok) {
-                const userData = await response.json();
-                const username = userData.username;
-
-                // Une al usuario a la sala con su nombre de usuario
-                socket.emit("join", { room: roomName, username });
-                console.log("Joining room:", roomName, "as user:", username);
-            } else {
-                console.error('Error fetching user:', response.status, response.statusText);
-            }
-        } catch (error) {
-            console.error('Error joining room:', error);
-        }
+    const handleJoinRoom = () => {
+        socket.emit("join", roomName);
+        console.log(roomName);
     };
 
     return (
@@ -60,11 +33,11 @@ export const RoomsPubliques = () => {
             <h1>Rooms</h1>
             <form className="form-room" >
                 <label htmlFor="text">Name Room</label>
-                <input value={roomName} onChange={(e) => setRoomsName(e.target.value)} type="text" />
-
+                <input value={roomName} onChange={(e) => setRoomsName(e.target.value)} type="text"/>
+                
             </form>
             <button type="submit" onClick={handleJoinRoom}>Create Public Room</button>
-
+            
             {/* <ul>
                 {rooms.map((room) =>(
                     <li key={room}>
