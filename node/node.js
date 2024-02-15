6bed5c5a502babaf570175e2ae5b7c14ed80afb4
Eric Gómez Vilà <105845066+EGVPedralbes@users.noreply.github.com>
2024-02-15 23:16:22 +0100

    NodeChange

--- node/node.js
+++ node/node.js
@@ -1,134 +1,138 @@
-const express = require("express")
+const express = require('express');
+const  cors  = require('cors');
+
+// const http = require('http');
+const { Server } = require('socket.io');
+
+
+
+// var partida = [];
 const app = express();
-const http = require("http");
-const cors = require("cors")
-const { Server } = require("socket.io");
-const { log } = require("console");
+const http = require('http').createServer(app);
 
-app.use(cors())
+var rooms = [];
 
-const server = http.createServer(app);
+// app.get('/', (_req, res) => {
+//   res.send("hola")
+// });
 
-const io = new Server(server, {
+const io=require('socket.io')(app, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
 
-const User = require('../back/strapi/src/api/user/services/user.js');
-
-app.get('/', (req, res) => {
-  res.send('eyyy');
-})
-
-//VARIABLES
-
-var rooms = []
-var lastRoom = 0
-
-const urlStrapi = 'http://localhost:1337/api/auth/local/';
+function getNewLevel(level){
+  let map = [];
+  fetch('http://localhost:1337/api/map', 
+    {
+      method: 'GET',
+      body: JSON.stringify(level),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      map = data;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+    return map;
+}   
 
 io.on('connection', (socket) => {
   console.log(`Connected: ${socket.id}`);
 
-  console.log('Salas: ', io.sockets.adapter.rooms);
-
-  socket.on('crearSala', (data) => {
-    console.log(data);
-    socket.join(data);
-    console.log('se ha unido a la sala', data);
-    console.log('Salas: ', io.sockets.adapter.rooms);
-
-    if (rooms.length == 0) {
-      rooms.push({ idRoom: lastRoom, name: data, users: [] });
-    } else {    
-      lastRoom++;
-      rooms.push({ idRoom: lastRoom, name: data, users: [] });
-    }
-    
-  });
-
-  socket.on("join", async (data) => {
-    console.log("Joining room:", data.room, "as user:", data.username);
-    
+  socket.on('join', (room) => {
+    console.log(`Socket ${socket.id} joining ${room}`);
     socket.join(data.room);
-
-    io.to(data.room).emit("userJoined", { username: data.username });
-
-    console.log('Salas:', io.sockets.adapter.rooms);
-  });
-    
-    
-    // const user = await User.obtenerInformacionUsuari('id', 'username');
-    
-    // if (rooms.length == 0) {
-    //   rooms.push({ idRoom: lastRoom, name: data, users: [user.username] });
-    // } else {
-    //   if (rooms[rooms.length - 1].users.length == 2) {
-    //     lastRoom++;
-    //     rooms.push({ idRoom: lastRoom, name: data, users: [user.username] });
-    //   } else {
-    //     rooms[rooms.length - 1].users.push(user.username);
-    //   }
-    // }
-
-    // // Busca la sala por nombre
-    // const room = rooms.find(room => room.name === 'nombre de la sala');
-
-    // // Si la sala existe, agrega el usuario
-    // if (room) {
-    //   room.users.push(user.username);
-    // } else {
-    //   // Si la sala no existe, crea una nueva sala con el usuario
-    //   rooms.push({ idRoom: lastRoom, name: data, users: [user.username] });
-    // }
-
-    // socket.join(user.username);
-    // console.log('se ha unido a la sala', user.username);
-    // socket.emit('updateUsers', []);
-    // console.log(rooms);
-    // io.to(user.username).emit('usersConnected');
-    // console.log('Salas: ', io.sockets.adapter.rooms);
-
-    // socket.emit('updateRooms', Object.keys(rooms));
-  // });
-  socket.on("sendMessage", (data) => {
-    io.to(data.room).emit("receiveMessage", data)
-    console.log(data);
-
-  })
-
-  socket.on('publicRooms', (rooms) => {
-
-  })
-
-  socket.on('disconnectUserRoom', () => {
-
-    for (const room in rooms) {
-      const index = rooms[room].indexOf(socket.id)
-      if (index !== -1) {
-        rooms[room].splice(index, 1)
-        io.to(room).emit("updateUsers", rooms[room])
-        break;
-      }
+    const index = rooms.findIndex((r) => r.id === room);
+    console.log('Index:', index);
+    if (index !== -1) {
+      rooms[index].users.push({ "user": data.username, "x": 0, "y": 0, "id": socket.id });
     }
+    io.to(data.id).emit('join', data);
+    // partida.push(data);
+    // console.log('data', data);
+    // console.log('partida', partida);
   });
 
+  socket.on('createRoom', (room) => {
+    if(room.password === '') room.password = null;
+    fetch('http://localhost:1337/api/map', 
+    {
+      method: 'GET',
+      body: JSON.stringify(1),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    rooms.push({ "users": [{ "user": room.username, "x": 0, "y": 0, "id": socket.id }], "password": room.password, "started": false, "level": 1, "id": lastRoom, "map":map });
+    socket.join(lastRoom);
+    lastRoom++;
+    socket.broadcast.emit('Updaterooms', rooms);
+  });
 
+  
+  socket.on('chat message', (dataMessage) => {
+    const { msg, room } = dataMessage;
+    console.log(`msg: ${msg}, room: ${room}`);
+    io.to(room).emit('chat message', msg);
+  });
+
+  socket.on('register', async (data) => {
+    console.log('register -->', data);
+    const response = await fetch('http://localhost:1337/api/auth/local/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    io.to(data.room).emit('register', data);
+  });
+
+  socket.on('login', async (data) => {
+    console.log('login -->', data);
+    const response = await fetch('http://localhost:1337/api/auth/local', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    io.to(data.room).emit('login', data);
+  });
+
+  socket.on('updatePosition', (data) => {
+    let room = rooms.findIndex((r) => r.id === data.room);
+    console.log('room:', data);
+    if (rooms[room].users[0].id === socket.id) {
+      rooms[room].users[0].x = data.x;
+      rooms[room].users[0].y = data.y;
+    } else {
+      rooms[room].users[1].x = data.x;
+      rooms[room].users[1].y = data.y;
+    }
+
+    io.to(data.room).emit('updatePosition', data);
+  });
+
+  socket.on('win',(data)=>{
+    let room = rooms.findIndex((r) => r.id === data.room);
+    rooms[room].level++;
+    rooms[room].map=getNewLevel(rooms[room].level);
+    io.to(data.room).emit('win', rooms[room]);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
 });
 
-server.listen(3001, 'localhost', () => {
-  console.log("\x1b[31m  ____  _            _           _      _           ");
-  console.log(" / ___|| |_ __ _  __| |_   _ ___| |_   (_)_ __ ___  ");
-  console.log("| |    | __/ _` |/ _` | | | / __| __|  | | '__/ _ \\ ");
-  console.log("| |___ | || (_| | (_| | |_| \\__ \\ |_   | | | |  __/ ");
-  console.log(" \\____| \\__\\__,_|\\__,_|\\__, |___/\\__|  |_|_|  \\___| ");
-  console.log("                        |___/                        ");
-  console.log("\x1b[34m   ____  _            _   _               _           ");
-  console.log("  / ___|| |_ __ _  __| | | |_   _    __| | ___  ___ ");
-  console.log("  \\___ \\| __/ _` |/ _` | | | | | |  / _` |/ _ \\/ __|");
-  console.log("   ___) | || (_| | (_| | | | | |_| | (_| |  __/\\__ \\");
-  console.log("  |____/ \\__\\__,_|\\__,_| |_|  \\__,_|\\__,_|\\___||___/");
-  console.log("\x1b[0mServer running at http://localhost:3001");
+http.listen(3001, 'localhost', () => {
+  console.log('Server running at http://localhost:3001');
 });
