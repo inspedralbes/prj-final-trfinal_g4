e2b953d0075a61a72a919865a3a1e7d969382fb8
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2024-04-23 12:55:36 +0200

    funciona el login con google

--- front/next/pages/login.jsx
+++ front/next/pages/login.jsx
@@ -1,18 +1,24 @@
-"use client";
-
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { login } from '../services/communicationManager';
 import { useRouter } from 'next/router';
-import { signIn, useSession, signOut } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const router = useRouter();
+  const [setSession] = useState(null);
 
   const session = useSession();
   console.log(session);
+
+  useEffect(() => {
+    // Si hay una sesión activa, redirige al usuario a la página de /rooms
+    if (session?.data) {
+      router.push('/rooms');
+    }
+  }, [session, router]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,8 +27,7 @@ function Login() {
       email: email,
       password: password
     };
-    
-    
+
     login(user)
       .then((data) => {
         localStorage.setItem('username', data.username);
@@ -31,13 +36,12 @@ function Login() {
       .catch(() => {
         alert('Error logging in');
       });
-    
-      signIn().then(() => {
-        router.push('/');
-      });
-      
-
   };
+
+  async function loginGoogle() {
+    await signIn('google');
+  }
+
   return (
     <div className="bg-gradient-to-r from-blue-400 to-indigo-500 min-h-screen flex flex-col justify-center items-center p-4">
       <form className="bg-white shadow-md rounded-lg px-8 py-6 max-w-md w-full" onSubmit={handleSubmit}>
@@ -45,7 +49,7 @@ function Login() {
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Correu electrònic</label>
           <input type="email" id="email" name="email"
-            value={email} onChange={(e) => setEmail(e.target.value)} 
+            value={email} onChange={(e) => setEmail(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
@@ -62,21 +66,20 @@ function Login() {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out hover:scale-110"
           >
             Iniciar Sessió
-            </button>
-            
-          
+          </button>
+
           {/* Enlace para registro */}
           <Link href="/register">
             <p className="text-blue-500 hover:text-blue-700 font-semibold text-sm mt-4">Registra't</p>
           </Link>
-
         </div>
       </form>
 
-      <button onClick={signIn} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out hover:scale-110 mt-4">
+      <button onClick={loginGoogle} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out hover:scale-110 mt-4">
         Iniciar Sessió amb Google
       </button>
-      <button onClick={()=> { signOut(); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out hover:scale-110 mt-4">
+
+      <button onClick={() => { signOut(); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out hover:scale-110 mt-4">
         Tancar Sessió
       </button>
     </div>
