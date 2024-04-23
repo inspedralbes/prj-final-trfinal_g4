509431ccb9ade04e0b5bb1b1d8fbeed4773c9eb3
julsluks <a22betvilver@inspedralbes.cat>
2024-04-23 22:51:21 +0200

    Refactorización de la sección de chat en la página del lobby

--- front/next/pages/lobby.jsx
+++ front/next/pages/lobby.jsx
@@ -66,7 +66,7 @@ const Lobby = () => {
             <div className='min-h-[600px] min-w-[850px] bg-black text-center'>
                 <h1 className='text-4xl font-bold tracking-wider my-5 animate-pulse'>ESPERANT ACOMPANYANT</h1>
                 {/* Chat section */}
-                <div className='flex flex-col min-w-[425px] mx-3 grid grid-cols-2 justify-center items-center'>
+                {/* <div className='flex flex-col min-w-[425px] mx-3 grid grid-cols-2 justify-center items-center'>
                     <div className='max-h-[350px] overflow-y-auto' >
                         <div className=" max-h-[350px] max-w-[250px]">
                             <div className="flex flex-col ">
@@ -83,7 +83,6 @@ const Lobby = () => {
                         </div>
                         <div className="flex flex-col items-center justify-center bg-slate-600 rounded-lg w-full max-w-xl ">
                             <div className="flex flex-col items-center bg-zinc-200 mx-8 ">
-                                
                             </div>
                         </div>
                     </div>
@@ -103,6 +102,49 @@ const Lobby = () => {
                         >
                             Enviar
                         </button>
+                    </div>
+                </div> */}
+                <div className='max-h-[350px] max-w-[400px]'>
+                    <div id='messages' className='flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-rhumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
+                        {/* Primer Mensaje */}
+                        <div className='chat-message'>
+                            <div className='flex items-end'>
+                                <div className='flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start'>
+                                    <div>
+                                        <span className='px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-200 text-gray-600'>
+                                            Hola! ¿Cómo estás?
+                                        </span>
+                                    </div>
+                                </div>
+                                <img src="/images/random.jpg" alt="Venti" className='w-6 h-6 rounded-full order-1' />
+                            </div>
+                        </div>
+                        {/* Segundo Mensaje */}
+                        <div className='chat-message'>
+                            <div className='flex items-end justify-end'>
+                                <div className='flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end'>
+                                    <div>
+                                        <span className='px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white'>
+                                            ¡Hola! Bien y tú?
+                                        </span>
+                                    </div>
+                                </div>
+                                <img src="/images/random.jpg" alt="Venti" className='w-6 h-6 rounded-full order-2' />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='border-t-2 border-gray-200 px-4 pt-4 mb-2 mb-16'>
+                        <div className='relative flex'>
+                            <span className='absolute inset-y-0 flex items-center'>
+                                <button className='inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300'>
+                                    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' className='h-6 w-6 text-gray-600'>
+                                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z'></path>
+                                    </svg>
+                                </button>
+                            </span>
+                            <input placeholder='Escriu un missatge...' type="text" className='focus:ring-red-500 focus:border-red-500 w-full focus:placeholder-gray-400 text-gray-600 placeholder-gray-300 pl-12 bg-gray-100 rounded-full py-3 border-gray-200' />
+                        </div>
                     </div>
                 </div>
             </div>
