d938974c66e869106543ee74501ae4584f6ce1a1
rublormar <a21rublormar@inspedralbes.cat>
2024-05-02 20:46:06 +0200

    probando tailwind css

---
+++ front/next/pages/adminPanel.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { FiUsers } from "react-icons/fi";
+
+const AdminPanel = () => {
+    return (
+        <div className="flex justify-center items-center h-screen bg-teal-900">
+            <div className="flex flex-col w-48 h-screen bg-teal-700">
+                <div className="flex flex-col items-center justify-center">
+                    <FiUsers style={{ color: 'white', fontSize: '2.5em', display: 'flex' }} />
+                </div>
+            </div>
+            <div className="flex-grow">
+            </div>
+
+
+        </div>
+    )
+};
+
+export default AdminPanel;
