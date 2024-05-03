f8d33587df1845e05862f84e200db4b30233c669
rublormar <a21rublormar@inspedralbes.cat>
2024-05-03 10:57:36 +0200

    iconos adminPanel

--- front/next/pages/adminPanel.jsx
+++ front/next/pages/adminPanel.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
 import { FiUsers } from "react-icons/fi";
 import { FaRegMap } from "react-icons/fa";
+import { TbMapExclamation } from "react-icons/tb";
+import { GoGraph } from "react-icons/go";
 
 const AdminPanel = () => {
     return (
         <div className="flex justify-center items-center h-screen bg-teal-900">
-            <div className="flex flex-col w-48 h-screen bg-teal-700">
-                <div className="flex flex-col items-center justify-center">
-                    <FiUsers style={{ color: 'white', fontSize: '2.5em', display: 'flex' }} />
+            <div className="flex flex-col w-48 h-screen bg-teal-700 items-center justify-center">
+                <div className="flex flex-col items-center justify-center pb-[25px]">
+                    <button className=''>
+                        <FiUsers style={{ color: 'white', fontSize: '3.5em', display: 'flex' }} />
+                    </button>
                 </div>
-                <div className="flex flex-col items-center justify-center">
-                    <FaRegMap style={{ color: 'white', fontSize: '2.5em', display: 'flex' }} />
+                <div className="flex flex-col items-center justify-center pb-[25px]">
+                    <FaRegMap style={{ color: 'white', fontSize: '3.5em', display: 'flex' }} />
+                </div>
+                <div className="flex flex-col items-center justify-center pb-[25px]">
+                    <TbMapExclamation style={{ color: 'white', fontSize: '3.5em', display: 'flex' }} />
+                </div><div className="flex flex-col items-center justify-center pb-[25px]">
+                    <GoGraph style={{ color: 'white', fontSize: '3.5em', display: 'flex' }} />
                 </div>
 
             </div>
