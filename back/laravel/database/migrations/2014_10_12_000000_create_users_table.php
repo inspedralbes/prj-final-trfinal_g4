ff9e988c5166bec5443118b7d0a99e5807a611db
SaraMartinez21 <a21sarmarbau@inspedralbes.cat>
2024-05-07 10:18:33 +0200

    Update 'users' table migration to make 'username' nullable

--- back/laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ back/laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username');
+            $table->string('username')->nullable()->default(null);
             $table->binary('image')->nullable()->default(null);
             $table->string('email')->unique();
             $table->string('password');
