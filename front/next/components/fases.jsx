4058f72ee8686b81e024463c82f5220949891d7e
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2024-05-09 12:51:14 +0200

    flechas imagenes done

--- front/next/components/fases.jsx
+++ front/next/components/fases.jsx
@@ -1,4 +1,3 @@
-// Fases.js
 import React, { useState } from 'react';
 
 const Fases = ({ fases }) => {
@@ -16,9 +15,9 @@ const Fases = ({ fases }) => {
         <div className="w-full sm:w-3/4 flex flex-col sm:flex-row items-center justify-center">
             <div className="flex flex-col sm:flex-row items-center justify-center sm:flex-wrap gap-x-4">
                 {fases.map((_, index) => (
-                    <div key={index} className="flex flex-col items-center relative my-4 sm:my-0 sm:mx-3.5">
+                    <div key={index} className={`flex flex-col items-center relative my-4 sm:my-0 sm:mx-3.5 ${index === currentPhaseIndex ? '' : 'hidden'}`}>
                         <div className="inline-block relative">
-                            <img src='/images/mapa.png' alt={`Fase ${index + 1}`} className="h-60 w-96 my-4 bg-zinc-400" />
+                            <img src={`/images/mapa${index + 1}.png`} alt={`Fase ${index + 1}`} className="h-60 w-96 my-4 bg-zinc-400" />
                             <button
                                 className="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded-l focus:outline-none absolute top-1/2 left-0 transform -translate-y-1/2 opacity-50"
                                 onClick={handlePrev}
