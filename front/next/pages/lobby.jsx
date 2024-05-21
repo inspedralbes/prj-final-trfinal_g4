ef93603e246e7918c7b083bedaaede146be9ea69
julsluks <a22betvilver@inspedralbes.cat>
2024-05-21 08:40:33 +0200

    Remove unnecessary code in lobby.jsx

--- front/next/pages/lobby.jsx
+++ front/next/pages/lobby.jsx
@@ -182,7 +182,6 @@ const Lobby = () => {
                                 <img src={userFromLocalStorage && userFromLocalStorage.image ? 'http://localhost:8000'+userFromLocalStorage.image : '/images/profiles/default.png'} alt="User" className='w-10 h-10 ml-2 rounded-full' />
                                     <p className='text-2xl ml-3 mt-1 mr-4'>{adminUser}</p>
                                 </div>
-
                                 { user == adminUser && room.users[0].state != 'Ready' && (
                                     <div id='buttons-check' className='flex items-center ml-auto'>
                                         <button className='bg-green-500 hover:bg-green-700 text-white font-bold inline-flex items-center justify-center px-4 py-2 mx-2 rounded-lg' onClick={() => userReady()}>
