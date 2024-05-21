e29bb7fcf8cdd5987381015ecc4e3b2213d36675
julsluks <a22betvilver@inspedralbes.cat>
2024-05-21 21:38:39 +0200

    chore: Refactor asset loading in Preloader.js

--- front/next/scenes/Preloader.js
+++ front/next/scenes/Preloader.js
@@ -1,38 +1,46 @@
 import Phaser from 'phaser';
 import useStore from '../src/store';
 
+const BASE_URL = 'http://localhost:8000';
+const ASSETS_PATH = '/assets/';
+
 export default class Preloader extends Phaser.Scene {
 
     constructor() {
         super('preloader');
     }
 
-
-
     preload() {
-        this.load.setBaseURL('http://localhost:8000/');
+        this.load.setBaseURL(BASE_URL);
         this.load.crossOrigin = 'anonymous';
         this.load.setCORS('anonymous');
-        this.load.atlas('character1', 'assets/character1.png', 'assets/character1.json');
-        this.load.atlas('flag-movement', 'assets/flag-movement.png', 'assets/flag-movement.json');
-        this.load.atlas('death', 'assets/appearing-character.png', 'assets/appearing-character.json');
-        this.load.atlas('platform', 'assets/platform.png', 'assets/platform.json');
-        this.load.image('tileset', 'assets/White-terrain.png');
-        // this.load.tilemapTiledJSON('mapa', mapRem);
-        this.load.image('logo', 'assets/Logo.png');
-        this.load.image('confetti', 'assets/confetti.png');
-        this.load.atlas('button', 'assets/pressButton.png', 'assets/pressButton.json')
-        // this.load.image('flag', 'assets/flag.png');
 
+        console.log('Loading assets...');   
+        this.loadAssets();
+    }
 
-        
-        this.load.tilemapTiledJSON('mapatuto', useStore.getState().gameData.maps[useStore.getState().gameData.currentMap].mapRoute);
+    loadAssets() {
+        try {
+            const { gameData } = useStore.getState();
+            const currentMap = gameData.maps[gameData.currentMap];
+
+            this.load.atlas('character1', `${ASSETS_PATH}character1.png`, `${ASSETS_PATH}character1.json`);
+            this.load.atlas('flag-movement', `${ASSETS_PATH}flag-movement.png`, `${ASSETS_PATH}flag-movement.json`);
+            this.load.atlas('death', `${ASSETS_PATH}appearing-character.png`, `${ASSETS_PATH}appearing-character.json`);
+            this.load.atlas('platform', `${ASSETS_PATH}platform.png`, `${ASSETS_PATH}platform.json`);
+            this.load.image('tileset', `${ASSETS_PATH}White-terrain.png`);
+            this.load.image('logo', `${ASSETS_PATH}Logo.png`);
+            this.load.image('confetti', `${ASSETS_PATH}confetti.png`);
+            this.load.atlas('button', `${ASSETS_PATH}pressButton.png`, `${ASSETS_PATH}pressButton.json`);
+            this.load.tilemapTiledJSON('mapatuto', currentMap.mapRoute);
+        } catch (error) {
+            console.error('Error loading assets:', error);
+        }
     }
 
     create() {
-
-        setTimeout(() => {
+        this.time.delayedCall(3000, () => {
             this.scene.start('gamehome');
-        }, 3000);
+        });
     }
 }
