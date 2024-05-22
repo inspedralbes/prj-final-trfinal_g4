0afaf2f9ffa8d76bd1a9d2352ac27e1d74e8c391
julsluks <a22betvilver@inspedralbes.cat>
2024-05-22 19:29:05 +0200

    chore: Update _app.jsx to handle route changes and socket disconnections

--- front/next/pages/_app.jsx
+++ front/next/pages/_app.jsx
@@ -1,16 +1,35 @@
-// _app.js o _app.jsx
-
 import tailwind from 'tailwindcss/tailwind.css';
-import React from 'react';
-import App from 'next/app';
-import {Providers} from '../app/Providers';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Providers } from '../app/Providers';
 import socket from '../services/sockets';
+import useStore from '../src/store';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
 
-  return <Providers>
-    <Component {...pageProps} />
-  </Providers>
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      const { room } = useStore.getState();
+      if (room && url !== '/lobby' && url !== '/game') {
+        socket.emit('exitRoom');
+        useStore.setState({ room: null });
+      }
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+    };
+  }, [router]);
+
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  );
 }
 
 export default MyApp;
