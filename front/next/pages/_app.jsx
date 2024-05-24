9f173c658b722befd12e45ac01ebe2e491ed3908
julsluks <a22betvilver@inspedralbes.cat>
2024-05-24 09:48:33 +0200

    Update _app.jsx and tailwind.config.js

--- front/next/pages/_app.jsx
+++ front/next/pages/_app.jsx
@@ -1,9 +1,11 @@
-import tailwind from 'tailwindcss/tailwind.css';
+// _app.jsx
+import 'tailwindcss/tailwind.css';
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Providers } from '../app/Providers';
 import socket from '../services/sockets';
 import useStore from '../src/store';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -26,9 +28,21 @@ function MyApp({ Component, pageProps }) {
   }, [router]);
 
   return (
-    <Providers>
-      <Component {...pageProps} />
-    </Providers>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet" />
+        <style jsx global>{`
+          html {
+            font-family: 'Pixelify Sans', Arial, sans-serif;
+          }
+        `}</style>
+      </Head>
+      <Providers>
+        <Component {...pageProps} />
+      </Providers>
+    </>
   );
 }
 
